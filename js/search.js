ce80e66cb9249530c0a43ae7a1d1f030df4168ca
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2022-05-30 15:30:27 -0300

    Fix #3

--- js/search.js
+++ js/search.js
@@ -1,39 +1,36 @@
-const searchData = await getSearchData();
-
-function searchApps(){
-    const filter = document.getElementById('search').value;
-    const matchs = searchData.filter(app => {
-        app.name.toLowerCase().includes(filter) || app.description.toLowerCase().includes(filter)
-    });
-
-    drawResult(matchs)
+async function searchApps(){
+    const searchData = await getSearchData();
+    const filter = document.getElementById('search').value.toLowerCase();
+    const matchs = searchData.filter(app => app.name.toLowerCase().includes(filter) || app.description.toLowerCase().includes(filter));
+    drawResult(matchs);
 }
 
 async function drawResult(data){
-    let categoryHTML = document.getElementById('menu-category');
-    let menuHTML = document.getElementById('menu-content');
+    const categoryHTML = document.getElementById('menu-category');
+    const menuHTML = document.getElementById('menu-content');
     if (document.getElementById('search').value == ''){
         drawMenu();
     } else if (data.length === 0){
         categoryHTML.innerHTML = '';
         menuHTML.innerHTML = 'No se encontraron apps';
     } else {
-        categoryHTML.innerHTML = '';
-        let myHTML;
-        for (app in data){
+        let myHTML = '<div class="container-fluid"><div class="row">';
+        for (app of data){
+            console.log(app)
             myHTML = myHTML.concat(`
             <button class="btn col-3 btn-app"
                 data-bs-toggle="tooltip"
                 data-bs-placement="top"
-                title="${apps[app].name}"
-                onclick="openApp('${apps[app].path}')">
-                <img src="file://${apps[app].icon}" class="img-fluid" style='width:100%;' />
-                <span class="name-app text-center">${apps[app].name}</span>
-                <span class='text-muted' style='display:none;'>${apps[app].description}</span>
-                <span style='display:none;'>${apps[app].keywords}</span>
+                title="${app.name}"
+                onclick="openApp('${app.path}')">
+                <img src="file://${app.icon}" class="img-fluid" style='width:100%;' />
+                <span class="name-app text-center">${app.name}</span>
+                <span class='text-muted' style='display:none;'>${app.description}</span>
+                <span style='display:none;'>${app.keywords}</span>
             </button>
             `);
         }
-        menuHTML.innerHTML = myHTML;
+        menuHTML.innerHTML = myHTML.concat("</div></div>");
+        categoryHTML.innerHTML = '';
     }
 }
