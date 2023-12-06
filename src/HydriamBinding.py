6ad28130c7a2a9ca7a5195b45335a8a00ddeb4f2
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2023-12-06 15:53:39 -0300

    Add power management functionality

--- src/HydriamBinding.py
+++ src/HydriamBinding.py
@@ -2,6 +2,7 @@ import os
 import json
 from PyQt6.QtCore import pyqtSlot, QObject
 from Vasak.system.VSKMenuManager import VSKMenuManager
+from Vasak.system.VSKPowerManager import VSKPowerManager
 
 class HydriamBinding(QObject):
   def __init__(self, window, app):
@@ -9,6 +10,7 @@ class HydriamBinding(QObject):
     self.window = window
     self.app = app
     self.menuManager = VSKMenuManager()
+    self.powerManager = VSKPowerManager()
 
   @pyqtSlot(result=str)
   def getHome(self):
@@ -26,3 +28,23 @@ class HydriamBinding(QObject):
   @pyqtSlot()
   def exit(self):
     self.app.quit()
+
+  @pyqtSlot()
+  def reboot(self):
+    self.powerManager.reboot()
+  
+  @pyqtSlot()
+  def shutdown(self):
+    self.powerManager.poweroff()
+  
+  @pyqtSlot()
+  def suspend(self):
+    self.powerManager.suspend()
+  
+  @pyqtSlot()
+  def logout(self):
+    self.powerManager.logout()
+
+  @pyqtSlot()
+  def lock(self):
+    self.powerManager.lock()
