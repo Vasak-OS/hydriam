d42a65440bc8b0a1fe3d7ce9c7442d1b2cb1665d
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2023-12-07 11:00:50 -0300

    Add keydown event listener to exit app on Escape key press

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -3,6 +3,12 @@ import { createPinia } from 'pinia';
 import App from './App.vue';
 
 const startApp = (channel: any) => {
+  document.onkeydown = (e) => {
+    if (e.key === 'Escape') {
+      channel.objects.vsk.exit();
+    }
+  };
+
   const app = createApp(App);
   app.config.globalProperties.$vsk = channel.objects.vsk;
   app.use(createPinia());
