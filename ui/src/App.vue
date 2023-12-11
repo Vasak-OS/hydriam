337ac161816c700b6b7c2c4a8ea08d041ad99a23
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2023-12-11 17:18:24 -0300

    add new skel

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -4,6 +4,13 @@ import SearchComponent from '@/components/SearchComponent.vue';
 import MenuSection from '@/components/section/MenuSection.vue';
 import FilterTab from '@/components/tab/FilterTab.vue';
 import UserInfo from '@/components/UserInfo.vue';
+import SessionButton from '@/components/button/SessionButton.vue';
+
+
+import shutdown from '@/assets/img/shutdown.svg';
+import reboot from '@/assets/img/reboot.svg';
+import logout from '@/assets/img/logout.svg';
+import suspend from '@/assets/img/suspend.svg';
 
 const menuData = {
   'Category 1': {
@@ -41,7 +48,11 @@ export default defineComponent({
   data() {
     return {
       filter: '',
-      menuData
+      menuData,
+      shutdown,
+      reboot,
+      logout,
+      suspend,
     };
   },
   created() {
@@ -51,7 +62,23 @@ export default defineComponent({
     async setMenu() {
       this.menuData = JSON.parse(await (this as any).$vsk.getMenuData());
       console.log(this.menuData);
-    }
+    },
+    async logoutF() {
+      await (this as any).$vsk.logout();
+      (this as any).$vsk.exit();
+    },
+    async shutdownF() {
+      await (this as any).$vsk.shutdown();
+      (this as any).$vsk.exit();
+    },
+    async rebootF() {
+      await (this as any).$vsk.reboot();
+      (this as any).$vsk.exit();
+    },
+    async suspendF() {
+      await (this as any).$vsk.suspend();
+      (this as any).$vsk.exit();
+    },
   },
   computed: {
     apps() {
@@ -68,25 +95,40 @@ export default defineComponent({
     SearchComponent,
     MenuSection,
     FilterTab,
-    UserInfo
+    UserInfo,
+    SessionButton
   }
 });
 </script>
 
 <template>
   <div class="row">
-    <div class="col-md-1">
+    <div class="col-md-4">
       <UserInfo />
     </div>
-    <div class="col-md-11">
+    <div class="col-md-4">
       <SearchComponent v-model:filter="filter" />
-      <template v-if="filter !== ''">
-        <FilterTab :apps="apps" :filter="filter" />
-      </template>
-      <template v-else>
-        <MenuSection :menuData="menuData" />
-      </template>
     </div>
+    <div class="col-md-4">
+      <SessionButton title="Shutdown" :img="shutdown" @click="logoutF" />
+      <SessionButton title="Reboot" :img="reboot" @click="shutdownF" />
+      <SessionButton title="Logout" :img="logout" @click="rebootF" />
+      <SessionButton title="Suspend" :img="suspend" @click="suspendF" />
+    </div>
+
+    <template v-if="filter !== ''">
+      <div class="col-12">
+        <FilterTab :apps="apps" :filter="filter" />
+      </div>
+    </template>
+    <template v-else>
+      <div class="col-md-4">
+        <MenuSection :menuData="menuData" />
+      </div>
+
+      <div class="col-md-4"></div>
+      <div class="col-md-4"></div>
+    </template>
   </div>
 </template>
 
