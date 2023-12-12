122df0bd8fc60c0f6d1336b339188a854260ee1e
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2023-12-12 11:56:34 -0300

    add new weather alpha widget

---
+++ ui/src/components/card/CurrentWeatherCard.vue
@@ -0,0 +1,46 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import WeatherIcon from '@/components/icon/WeatherIcon.vue';
+
+export default defineComponent({
+  name: 'CurrentWeatherCard',
+  props: {
+    current: {
+      type: Object,
+      required: true
+    },
+    units: {
+      type: Object,
+      required: true
+    },
+    dayOrNight: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    getTemUnit() {
+      return this.units.temperature_2m;
+    },
+    getTemperature() {
+      return this.current.temperature_2m;
+    }
+  },
+  components: {
+    WeatherIcon
+  }
+});
+</script>
+
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-6 temp-text">
+        <h3 class="text-center">{{ getTemperature }} {{ getTemUnit }}</h3>
+      </div>
+      <div class="col-6">
+        <WeatherIcon :code="current.weather_code" :dayOrNight="dayOrNight" />
+      </div>
+    </div>
+  </div>
+</template>
---
+++ ui/src/components/card/DailyWeatherCard.vue
@@ -0,0 +1,42 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import WeatherIcon from '@/components/icon/WeatherIcon.vue';
+
+export default defineComponent({
+  name: 'DailyWeatherCard',
+  props: {
+    min: {
+      type: Number,
+      required: true
+    },
+    max: {
+      type: Number,
+      required: true
+    },
+    units: {
+      type: Object,
+      required: true
+    },
+    dayOrNight: {
+      type: String,
+      required: true
+    },
+    weatherCode: {
+      type: Number,
+      required: true
+    }
+  },
+  components: {
+    WeatherIcon
+  }
+});
+</script>
+<template>
+  <div class="col text-center">
+    <div class="col-12">
+      <WeatherIcon :code="weatherCode" :dayOrNight="dayOrNight" />
+    </div>
+    <div class="col-12 temp-text">{{ max }}</div>
+    <div class="col-12 temp-text">{{ min }}</div>
+  </div>
+</template>
